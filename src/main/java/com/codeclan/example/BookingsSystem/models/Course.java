bc39d5c0308fd65736ed3704b3524cfc63d34e17
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-04-18 19:32:02 +0100

    Course model constructor completed

--- src/main/java/com/codeclan/example/BookingsSystem/models/Course.java
+++ src/main/java/com/codeclan/example/BookingsSystem/models/Course.java
@@ -1,21 +1,77 @@
 package com.codeclan.example.BookingsSystem.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
+
+@Entity
+@Table(name = "courses")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "town")
     private String town;
 
+    @Column(name = "star_rating")
     private int starRating;
 
     private List<Booking> bookings;
 
-    public Course(String name, String town, int starRating) {
+    public Course(String name, String town, int rating) {
         this.name = name;
+        this.town = town.toLowerCase();
+        this.starRating = rating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
         this.town = town;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
         this.starRating = starRating;
     }
 
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
 }
