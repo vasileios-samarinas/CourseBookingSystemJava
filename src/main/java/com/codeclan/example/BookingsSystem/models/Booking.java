e6cffb33159f807963ee2ea74702d5c9f62d2f34
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-04-18 19:36:25 +0100

    Booking class constructor completed

--- src/main/java/com/codeclan/example/BookingsSystem/models/Booking.java
+++ src/main/java/com/codeclan/example/BookingsSystem/models/Booking.java
@@ -1,4 +1,50 @@
 package com.codeclan.example.BookingsSystem.models;
 
 public class Booking {
+
+    private Long id;
+
+    private String date;
+
+    private Course course;
+
+    private Customer customer;
+
+    public Booking(String date, Course course, Customer customer) {
+        this.date = date;
+        this.course = course;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
---
+++ src/main/java/com/codeclan/example/BookingsSystem/models/Customer.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.BookingsSystem.models;
+
+public class Customer {
+}
