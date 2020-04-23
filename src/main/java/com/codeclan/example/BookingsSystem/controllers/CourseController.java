eda864a2a9c52080fe4310a0b8c3765d4ac3670b
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-04-23 17:07:37 +0100

    RESTful routes completed for CourseController

--- src/main/java/com/codeclan/example/BookingsSystem/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/BookingsSystem/controllers/CourseController.java
@@ -1,4 +1,50 @@
 package com.codeclan.example.BookingsSystem.controllers;
 
+
+import com.codeclan.example.BookingsSystem.models.Course;
+import com.codeclan.example.BookingsSystem.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/courses")
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses(){
+
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable Long id) {
+        return new ResponseEntity(courseRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> postCourse(@RequestBody Course course){
+        courseRepository.save(course);
+        return new ResponseEntity<>(course, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Course> putCourse(@RequestBody Course course){
+        courseRepository.save(course);
+        return new ResponseEntity<>(course, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Long> deleteCourse(@PathVariable Long id){
+        courseRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/codeclan/example/BookingsSystem/repositories/CourseRepository.java
+++ src/main/java/com/codeclan/example/BookingsSystem/repositories/CourseRepository.java
@@ -3,5 +3,8 @@ package com.codeclan.example.BookingsSystem.repositories;
 import com.codeclan.example.BookingsSystem.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
 }
