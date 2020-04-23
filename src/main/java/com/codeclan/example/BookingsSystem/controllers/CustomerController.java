e7eb83f1399cde63bba118e69df7e589941978db
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-04-23 17:12:05 +0100

    RESTful routes completed for CustomerController

--- src/main/java/com/codeclan/example/BookingsSystem/controllers/CustomerController.java
+++ src/main/java/com/codeclan/example/BookingsSystem/controllers/CustomerController.java
@@ -1,4 +1,49 @@
 package com.codeclan.example.BookingsSystem.controllers;
 
+import com.codeclan.example.BookingsSystem.models.Customer;
+import com.codeclan.example.BookingsSystem.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long id) {
+        return new ResponseEntity(customerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> postCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Customer> putCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Long> deleteCustomer(@PathVariable Long id){
+        customerRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+
 }
